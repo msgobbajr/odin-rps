4f9c33dd057273ddd9a0c9e2335a86705c99dbfe
Mark Sgobba <74211472+msgobbajr@users.noreply.github.com>
2025-01-22 22:30:59 -0500

    Add player choice function
    Added function to let user make choice for game

--- js/rps.js
+++ js/rps.js
@@ -1,5 +1,4 @@
-function getComputerChoice ()
-{
+function getComputerChoice (){
     let tempNum = Math.floor(Math.random()*3)+1;
     if (tempNum == 1) {
         return "rock";
@@ -12,4 +11,12 @@ function getComputerChoice ()
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice (){
+    let pChoice = prompt("Enter rock/paper/scissors?");
+    while (pChoice !== "rock" && pChoice !== "paper" && pChoice !== "scissors"){
+        pChoice = prompt("Check spelling and enter rock/paper/scissors")
+    }
+    return pChoice;
+}
+
+console.log(getHumanChoice());
