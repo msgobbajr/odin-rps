54d8b8374f069d759a2a63adb989f137bb0dac8c
Mark Sgobba <74211472+msgobbajr@users.noreply.github.com>
2025-01-23 18:34:08 -0500

    Add function to play round
    Add playRound function that compares inputs from both

--- js/rps.js
+++ js/rps.js
@@ -1,6 +1,11 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
+
 function getComputerChoice (){
     let tempNum = Math.floor(Math.random()*3)+1;
     if (tempNum == 1) {
@@ -15,11 +20,58 @@ function getComputerChoice (){
 }
 
 function getHumanChoice (){
+    //Get human choice
     let pChoice = prompt("Enter rock/paper/scissors?");
+    //Change to lower case to get around sensitivity
+    pChoice=pChoice.toLowerCase();
+    //Loop to check if word is valid
     while (pChoice !== "rock" && pChoice !== "paper" && pChoice !== "scissors"){
         pChoice = prompt("Check spelling and enter rock/paper/scissors")
+        pChoice=pChoice.toLowerCase();
     }
     return pChoice;
 }
 
-console.log(getHumanChoice());
+function playRound (humanChoice, computerChoice){
+    console.log("You picked "+humanChoice+".");
+    console.log("They picked "+computerChoice+".");
+    if(computerChoice==="rock"){
+        if(humanChoice==="rock"){
+            console.log("It's a tie!");
+        }
+        else if(humanChoice==="paper"){
+            console.log("You won!");
+            humanScore+=1;
+        }
+        else {
+            console.log("You lost!");
+            computerScore+=1;
+        }
+    }
+    else if(computerChoice==="paper"){
+        if(humanChoice==="rock"){
+            console.log("You lost!");
+            computerScore+=1;
+        }
+        else if(humanChoice==="paper"){
+            console.log("It's a tie!");
+        }
+        else {
+            console.log("You won!");
+            humanScore+=1;
+        }
+    }
+    else {
+        if(humanChoice==="rock"){
+            console.log("You won!");
+            humanScore+=1;
+        }
+        else if(humanChoice==="paper"){
+            console.log("You lost!");
+            computerScore+=1;
+        }
+        else {
+            console.log("It's a tie!");
+        }
+    }
+}
