a7f4e3eeccaf8cb2bd71befda39aef292f471b14
Mark Sgobba <74211472+msgobbajr@users.noreply.github.com>
2025-01-22 22:21:09 -0500

    Add logic for computer choice
    Add getComputerChoice funtion to compute rng result

--- js/rps.js
+++ js/rps.js
@@ -1,4 +1,15 @@
-function test ()
+function getComputerChoice ()
 {
-    return;
+    let tempNum = Math.floor(Math.random()*3)+1;
+    if (tempNum == 1) {
+        return "rock";
+    }
+    else if (tempNum == 2) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
 }
+
+console.log(getComputerChoice());
