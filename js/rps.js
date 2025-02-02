fd96ca178f9893ad1be9644e4dbbe0a9184f5e77
Mark Sgobba <74211472+msgobbajr@users.noreply.github.com>
2025-02-02 00:24:16 -0500

    Added UI for game
    Refactor code to add Div manipulation for UI of game

--- js/rps.js
+++ js/rps.js
@@ -16,94 +16,82 @@ function getComputerChoice (){
     }
 }
 
-//Get player choice for round
-function getHumanChoice (){
-    //Get human choice
-    let pChoice = prompt("Enter rock/paper/scissors?");
-    //Change to lower case to get around sensitivity
-    pChoice=pChoice.toLowerCase();
-    //Loop to check if word is valid
-    while (pChoice !== "rock" && pChoice !== "paper" && pChoice !== "scissors"){
-        //Get input again if incorrect
-        pChoice = prompt("Check spelling and enter rock/paper/scissors")
-        //Convert to lower case
-        pChoice=pChoice.toLowerCase();
-    }
-    //Return result of human choice
-    return pChoice;
-}
-
 //Play single round of game
-function playRound (){
+function playRound (buttonClicked){
     //get human and computer choice for round
-    humanChoice=getHumanChoice();
+    humanChoice=buttonClicked;
     computerChoice=getComputerChoice();
+    const resultP = document.getElementById('fRes');
+    const playerP = document.getElementById('pRes');
+    const compP = document.getElementById('cRes');
     //print what each player picked
-    console.log("You picked "+humanChoice+".");
-    console.log("They picked "+computerChoice+".");
+    playerP.textContent = "You picked "+humanChoice+".";
+    compP.textContent = "They picked "+computerChoice+".";
     //nested if to determine results
     if(computerChoice==="rock"){
         if(humanChoice==="rock"){
-            console.log("The round is a tie!");
+            resultP.textContent = "The round is a tie!";
         }
         else if(humanChoice==="paper"){
-            console.log("You won the round!");
+            resultP.textContent = "You won the round!";
             humanScore+=1;
         }
         else {
-            console.log("You lost the round!");
+            resultP.textContent = "You lost the round!";
             computerScore+=1;
         }
     }
     else if(computerChoice==="paper"){
         if(humanChoice==="rock"){
-            console.log("You lost the round!");
+            resultP.textContent = "You lost the round!";
             computerScore+=1;
         }
         else if(humanChoice==="paper"){
-            console.log("The round is a tie!");
+            resultP.textContent = "The round is a tie!";
         }
         else {
-            console.log("You won the round!");
+            resultP.textContent = "You won the round!";
             humanScore+=1;
         }
     }
     else {
         if(humanChoice==="rock"){
-            console.log("You won the round!");
+            resultP.textContent = "You won the round!";
             humanScore+=1;
         }
         else if(humanChoice==="paper"){
-            console.log("You lost the round!");
+            resultP.textContent = "You lost the round!";
             computerScore+=1;
         }
         else {
-            console.log("The round is a tie!");
+            resultP.textContent = "The round is a tie!";
         }
     }
-    //increment round
-    round+=1;
+    if (humanScore == 5){
+        resultP.textContent = "";
+        playerP.textContent = "";
+        compP.textContent = "";
+        scoreP.textContent = "You won the game " + humanScore + "-" + computerScore + "! Congrats!";
+        humanScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore == 5){
+        resultP.textContent = "";
+        playerP.textContent = "";
+        compP.textContent = "";
+        scoreP.textContent = "You lost the game " + humanScore + "-" + computerScore + "! Try again!";
+        humanScore = 0;
+        computerScore = 0;
+    }
+    else scoreP.textContent = "Computer - " + computerScore + " You - " + humanScore;
 }
 
-function playGame (){
-    //Set variables for game score
-    humanScore = 0;
-    computerScore = 0;
-    round = 1;
-    //Loop to play 5 rounds of game
-    while(round<6){
-        playRound();
-    }
-    //Check who won after 5 rounds and print result
-    if (humanScore > computerScore){
-        console.log("You won the game! "+humanScore+"-"+computerScore);
-    }
-    else if (computerScore > humanScore){
-        console.log("You lost the game! "+humanScore+"-"+computerScore);
-    }
-    else{
-        console.log("The game is a tie! "+humanScore+"-"+computerScore);
-    }
-}
-
-playGame();
+let humanScore = 0;
+let computerScore = 0;
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const scoreP = document.getElementById("score");
+rockButton.addEventListener('click', () => playRound('rock'));
+paperButton.addEventListener('click', () => playRound('paper'));
+scissorsButton.addEventListener('click', () => playRound('scissors'));
